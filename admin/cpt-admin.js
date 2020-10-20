ea3745cbab5540aab0c72df2f4661ffe14726169
samglover <sam@samglover.net>
2020-10-20 17:45:36 -0500

    1.1.0

--- admin/cpt-admin.js
+++ admin/cpt-admin.js
@@ -17,4 +17,27 @@
 
   });
 
+
+  // Admin Delete-Client Warning
+  let deleteClientLink    = $( '#cpt-delete-client-link' );
+
+  let cptAdminModal       = $( '.cpt-admin-modal' );
+  let adminModalScreen    = $( '.cpt-admin-modal-screen' );
+  let deleteCancelButton  = $( '.cpt-cancel-delete-client' );
+
+  deleteClientLink.click( function() {
+
+    cptAdminModal.show();
+    adminModalScreen.show();
+
+  });
+
+  deleteCancelButton.click( function() {
+
+    cptAdminModal.hide();
+    adminModalScreen.hide();
+
+  });
+
+
 })( jQuery );
