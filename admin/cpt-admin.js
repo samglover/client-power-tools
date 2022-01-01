324a65d933f5adb7d9a29800f646f4bdb6081ca6
samglover <sam@samglover.net>
2022-01-01 10:39:48 -0600

    Fixed a JS error when the page is not a client page.

--- admin/cpt-admin.js
+++ admin/cpt-admin.js
@@ -5,12 +5,16 @@ let cptAdminModal       = document.querySelector( '.cpt-admin-modal' );
 let adminModalScreen    = document.querySelector( '.cpt-admin-modal-screen' );
 let deleteCancelButton  = document.querySelector( '.cpt-cancel-delete-client' );
 
-deleteClientLink.addEventListener( 'click', function() {
-  cptAdminModal.style.display = 'grid';
-  adminModalScreen.style.display = 'block';
-});
+if ( deleteClientLink ) {
+  deleteClientLink.addEventListener( 'click', function() {
+    cptAdminModal.style.display = 'grid';
+    adminModalScreen.style.display = 'block';
+  });
+}
 
-deleteCancelButton.addEventListener( 'click', function() {
-  cptAdminModal.style.display = 'none';
-  adminModalScreen.style.display = 'none';
-});
+if ( deleteCancelButton ) {
+  deleteCancelButton.addEventListener( 'click', function() {
+    cptAdminModal.style.display = 'none';
+    adminModalScreen.style.display = 'none';
+  });
+}
