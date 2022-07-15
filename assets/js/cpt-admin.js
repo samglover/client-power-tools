b9647ef3ba7fca6030a54f8ea5c840dc85a556e5
Sam Glover <sam@samglover.net>
2022-07-15 13:05:42 -0500

    Misc. code cleanup and reformatting.

--- assets/js/cpt-admin.js
+++ assets/js/cpt-admin.js
@@ -1,19 +1,19 @@
 // Admin Delete-Client Warning
-let deleteClientLink    = document.querySelector( '#cpt-delete-client-link' );
+let deleteClientLink    = document.querySelector('#cpt-delete-client-link');
 
-let cptAdminModal       = document.querySelector( '.cpt-admin-modal' );
-let adminModalScreen    = document.querySelector( '.cpt-admin-modal-screen' );
-let deleteCancelButton  = document.querySelector( '.cpt-cancel-delete-client' );
+let cptAdminModal       = document.querySelector('.cpt-admin-modal');
+let adminModalScreen    = document.querySelector('.cpt-admin-modal-screen');
+let deleteCancelButton  = document.querySelector('.cpt-cancel-delete-client');
 
 if ( deleteClientLink ) {
-  deleteClientLink.addEventListener( 'click', function() {
+  deleteClientLink.addEventListener('click', function() {
     cptAdminModal.style.display = 'grid';
     adminModalScreen.style.display = 'block';
   });
 }
 
 if ( deleteCancelButton ) {
-  deleteCancelButton.addEventListener( 'click', function() {
+  deleteCancelButton.addEventListener('click', function() {
     cptAdminModal.style.display = 'none';
     adminModalScreen.style.display = 'none';
   });
