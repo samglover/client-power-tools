c2f0e9586afac25abe07d84d6c329d6bf8206a12
samglover <sam@samglover.net>
2022-11-21 16:55:36 -0600

    Revert "1.5.4: New filter: It is now possible to add fields to the Edit Clients dialog using the client_fields filter."
    This reverts commit 3bbdccfdc7fef7df94d75c67d46283fc1111335f.

--- admin/cpt-edit-client-form.php
+++ admin/cpt-edit-client-form.php
@@ -1,51 +1,43 @@
 <?php namespace Client_Power_Tools\Core\Admin; ?>
 
-<?php
-  $fields = apply_filters('client_fields', [
-      [
-        'id' => 'first_name',
-        'label' => 'First Name',
-        'required' => true,
-        'type' => 'text',
-      ],
-      [
-        'id' => 'last_name',
-        'label' => 'Last Name',
-        'required' => true,
-        'type' => 'text',
-      ],
-      [
-        'id' => 'email',
-        'label' => 'Email Address',
-        'required' => true,
-        'type' => 'email',
-      ],
-      [
-        'id' => 'client_id',
-        'label' => 'Client ID',
-        'required' => false,
-        'type' => 'text',
-      ],
-    ],
-  );
-?>
-
 <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="POST">
   <?php wp_nonce_field('cpt_client_updated', 'cpt_client_updated_nonce'); ?>
   <input name="action" value="cpt_client_updated" type="hidden">
   <input name="clients_user_id" value="<?php echo $client_data['user_id']; ?>" type="hidden">
   <table class="form-table" role="presentation">
     <tbody>
-      <?php foreach($fields as $field) { ?>
-        <tr>
-          <th scope="row">
-            <label for="<?php echo $field['id']; ?>"><?php echo $field['label']; ?><br /><small>(<?php echo $field['required'] ? 'required' : 'optional'; ?>)</small></label>
-          </th>
-          <td>
-            <input name="<?php echo $field['id']; ?>" id="<?php echo $field['id']; ?>" class="regular-text" type="<?php echo $field['type']; ?>" data-required="<?php echo $field['required'] ? 'true' : 'false'; ?>" value="<?php echo $client_data[$field['id']]; ?>">
-          </td>
-        </tr>
-      <?php } ?>
+      <tr>
+        <th scope="row">
+          <label for="first_name">First Name<br /><small>(required)</small></label>
+        </th>
+        <td>
+          <input name="first_name" id="first_name" class="regular-text" type="text" data-required="true" value="<?php echo $client_data['first_name']; ?>">
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <label for="last_name">Last Name<br /><small>(required)</small></label>
+        </th>
+        <td>
+          <input name="last_name" id="last_name" class="regular-text" type="text" data-required="true" value="<?php echo $client_data['last_name']; ?>">
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <label for="email">Email Address<br /><small>(required)</small></label>
+        </th>
+        <td>
+          <input name="email" id="email" class="regular-text" type="text" data-required="true" autocapitalize="none" autocorrect="off" value="<?php echo $client_data['email']; ?>">
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">
+          <label for="client_id">Client ID<br /><small>(optional)</small></label>
+        </th>
+        <td>
+          <input name="client_id" id="client_id" class="regular-text" type="text" autocapitalize="none" autocorrect="off" value="<?php echo $client_data['client_id']; ?>">
+        </td>
+      </tr>
       <tr>
         <th scope="row">
           <label for="client_manager">Client Manager</label>
