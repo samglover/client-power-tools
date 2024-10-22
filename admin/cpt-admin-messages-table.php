60da9f1f5800ee831212861a4c376ee0a08666c9
samglover <sam@samglover.net>
2024-10-22 00:41:15 -0500

    Convert indents to tabs and apply phpcs formatting

--- admin/cpt-admin-messages-table.php
+++ admin/cpt-admin-messages-table.php
@@ -6,205 +6,217 @@
  */
 
 namespace Client_Power_Tools\Core\Admin;
+
 use Client_Power_Tools\Core\Common;
 
-class Message_List_Table extends \WP_List_Table  {
-  function __construct() {
-    global $status, $page;
-    parent::__construct([
-      'singular'  => 'message',
-      'plural'    => 'messages',
-      'ajax'      => false,
-    ]);
-  }
+class Message_List_Table extends \WP_List_Table {
+	function __construct() {
+		global $status, $page;
+		parent::__construct(
+			array(
+				'singular' => 'message',
+				'plural'   => 'messages',
+				'ajax'     => false,
+			)
+		);
+	}
 
-  /**
-   * Column Default
-   *
-   * @param array $item A singular item (one full row's worth of data)
-   * @param array $column_name The name/slug of the column to be processed
-   * @return string Text or HTML to be placed inside the column <td>
-   */
-  function column_default($item, $column_name) {
-    return;
-  }
-
-
-  /**
-   * Checkboxes
-   *
-   * @see WP_List_Table::::single_row_columns()
-   * @param array $item A singular item (one full row's worth of data)
-   * @return string Text to be placed inside the column <td>
-   */
-  function column_cb($item) {
-    return sprintf(
-      '<input type="checkbox" name="%1$s[]" value="%2$s" />',
-      /* $1%s */ $this->_args['singular'],
-      /* $2%s */ $item['ID']
-    );
-  }
+	/**
+	 * Column Default
+	 *
+	 * @param array $item A singular item (one full row's worth of data)
+	 * @param array $column_name The name/slug of the column to be processed
+	 * @return string Text or HTML to be placed inside the column <td>
+	 */
+	function column_default( $item, $column_name ) {
+		return;
+	}
 
 
-  /**
-   * Client Column Method
-   */
-  function column_client($item) {
-    // Return the contents.
-    return sprintf('<strong>%1$s</strong> %2$s',
-      /* $1%s */ $item['client_name'],
-      /* $2%s */ $item['client_id'] ? '<span style="color:silver">(' . $item['client_id'] . ')</span>' : '',
-    );
-  }
-
-  /**
-   * Sender Column Method
-   */
-  function column_sender($item) {
-    return sprintf($item['sender']);
-  }
+	/**
+	 * Checkboxes
+	 *
+	 * @see WP_List_Table::::single_row_columns()
+	 * @param array $item A singular item (one full row's worth of data)
+	 * @return string Text to be placed inside the column <td>
+	 */
+	function column_cb( $item ) {
+		return sprintf(
+			'<input type="checkbox" name="%1$s[]" value="%2$s" />',
+			/* $1%s */ $this->_args['singular'],
+			/* $2%s */ $item['ID']
+		);
+	}
 
 
-  /**
-   * Subject Column Method
-   */
-  function column_subject($item) {
-    $clients_url  = add_query_arg('user_id', $item['clients_user_id'], esc_url(admin_url('admin.php?page=cpt')));
-    $msg_url      = $clients_url . '&paged=' . cpt_get_message_pagenum($item['clients_user_id'], $item['ID']) .  '#cpt-message-' . $item['ID'];
+	/**
+	 * Client Column Method
+	 */
+	function column_client( $item ) {
+		// Return the contents.
+		return sprintf(
+			'<strong>%1$s</strong> %2$s',
+			/* $1%s */ $item['client_name'],
+			/* $2%s */ $item['client_id'] ? '<span style="color:silver">(' . $item['client_id'] . ')</span>' : '',
+		);
+	}
 
-    // Return the contents.
-    return sprintf('<strong><a href="%1$s">%2$s</a></strong>',
-      /* $1%s */ $msg_url,
-      /* $2%s */ $item['subject'],
-   );
-  }
-
-
-  /**
-   * Date Column Method
-   */
-  function column_date($item) {
-    return sprintf($item['date']);
-  }
+	/**
+	 * Sender Column Method
+	 */
+	function column_sender( $item ) {
+		return sprintf( $item['sender'] );
+	}
 
 
-  /**
-   * Get Columns
-   *
-   * @see WP_List_Table::::single_row_columns()
-   * @return array An associative array containing column information:
-   * 'slugs' => 'Visible Titles'.
-   */
-  function get_columns() {
-    $columns = [
-      // 'cb'      => '<input type="checkbox" />',
-      'client'  => 'Client',
-      'sender'  => 'Sender',
-      'subject' => 'Subject',
-      'date'    => 'Date',
-    ];
-   return $columns;
-  }
+	/**
+	 * Subject Column Method
+	 */
+	function column_subject( $item ) {
+		$clients_url = add_query_arg( 'user_id', $item['clients_user_id'], esc_url( admin_url( 'admin.php?page=cpt' ) ) );
+		$msg_url     = $clients_url . '&paged=' . cpt_get_message_pagenum( $item['clients_user_id'], $item['ID'] ) . '#cpt-message-' . $item['ID'];
+
+		// Return the contents.
+		return sprintf(
+			'<strong><a href="%1$s">%2$s</a></strong>',
+			/* $1%s */ $msg_url,
+			/* $2%s */ $item['subject'],
+		);
+	}
 
 
-  /**
-   * Sortable Columns
-   */
-  function get_sortable_columns() {
-    return; // Remove this line to enable sortable columns.
-    $sortable_columns = [
-      'date' => ['msg_count', true],
-    ];
-    return $sortable_columns;
-  }
+	/**
+	 * Date Column Method
+	 */
+	function column_date( $item ) {
+		return sprintf( $item['date'] );
+	}
 
 
-  /**
-   * Bulk Actions
-   *
-   * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
-   */
-  function get_bulk_actions() {
-    return; // Remove this line to enable bulk actions.
-    $actions = [
-      'delete'  => 'Delete',
-    ];
-    return $actions;
-  }
+	/**
+	 * Get Columns
+	 *
+	 * @see WP_List_Table::::single_row_columns()
+	 * @return array An associative array containing column information:
+	 * 'slugs' => 'Visible Titles'.
+	 */
+	function get_columns() {
+		$columns = array(
+			// 'cb'      => '<input type="checkbox" />',
+			'client'  => 'Client',
+			'sender'  => 'Sender',
+			'subject' => 'Subject',
+			'date'    => 'Date',
+		);
+		return $columns;
+	}
 
 
-  function process_bulk_action() {
-    $action = $this->current_action();
-    switch ($action) {
-      case 'delete':
-        wp_die('Delete something.');
-        break;
-      default:
-        return;
-        break;
-    }
-    return;
-  }
+	/**
+	 * Sortable Columns
+	 */
+	function get_sortable_columns() {
+		return; // Remove this line to enable sortable columns.
+		$sortable_columns = array(
+			'date' => array( 'msg_count', true ),
+		);
+		return $sortable_columns;
+	}
 
 
-  /**
-   * Prepare Data for Display
-   */
-  function prepare_items() {
-    /**
-     * Column Headers
-     */
-    $columns = $this->get_columns();
-    $hidden = [];
-    $sortable = $this->get_sortable_columns();
+	/**
+	 * Bulk Actions
+	 *
+	 * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
+	 */
+	function get_bulk_actions() {
+		return; // Remove this line to enable bulk actions.
+		$actions = array(
+			'delete' => 'Delete',
+		);
+		return $actions;
+	}
 
-    $this->_column_headers = [$columns, $hidden, $sortable];
-    $this->process_bulk_action();
 
-    /**
-     * Query Messages
-     */
-    $data = [];
+	function process_bulk_action() {
+		$action = $this->current_action();
+		switch ( $action ) {
+			case 'delete':
+				wp_die( 'Delete something.' );
+				break;
+			default:
+				return;
+				break;
+		}
+		return;
+	}
 
-    // Creates the data set.
-    $cpt_messages = new \WP_Query([
-      'post_type' => 'cpt_message',
-      'posts_per_page' => -1,
-    ]);
 
-    if ($cpt_messages->have_posts()) : while ($cpt_messages->have_posts()) : $cpt_messages->the_post();
-      $post_id = get_the_ID();
-      $clients_user_id = get_post_meta($post_id, 'cpt_clients_user_id', true);
+	/**
+	 * Prepare Data for Display
+	 */
+	function prepare_items() {
+		/**
+		 * Column Headers
+		 */
+		$columns  = $this->get_columns();
+		$hidden   = array();
+		$sortable = $this->get_sortable_columns();
 
-      $data[] = [
-        'ID' => $post_id,
-        'client_name' => Common\cpt_get_client_name($clients_user_id),
-        'clients_user_id' => $clients_user_id,
-        'client_id' => get_user_meta($clients_user_id, 'cpt_client_id', true),
-        'sender' => get_the_author(),
-        'subject' => get_the_title() ? get_the_title() : '[Message from ' . get_the_author() . ']',
-        'date' => get_the_date(),
-      ];
-    endwhile; endif;
+		$this->_column_headers = array( $columns, $hidden, $sortable );
+		$this->process_bulk_action();
 
-    /**
-     * Pagination
-     */
-    $total_items = count($data);
-    $per_page = 25;
-    $current_page = $this->get_pagenum();
-    $data = array_slice($data, (($current_page - 1) * $per_page), $per_page);
+		/**
+		 * Query Messages
+		 */
+		$data = array();
 
-    $this->set_pagination_args([
-      'total_items' => $total_items,
-      'per_page' => $per_page,
-      'total_pages' => ceil($total_items / $per_page),
-    ]);
+		// Creates the data set.
+		$cpt_messages = new \WP_Query(
+			array(
+				'post_type'      => 'cpt_message',
+				'posts_per_page' => -1,
+			)
+		);
 
-    /**
-     * $this->items contains the data that will actually be displayed on the
-     * current page.
-     */
-    $this->items = $data;
-  }
+		if ( $cpt_messages->have_posts() ) :
+			while ( $cpt_messages->have_posts() ) :
+				$cpt_messages->the_post();
+				$post_id         = get_the_ID();
+				$clients_user_id = get_post_meta( $post_id, 'cpt_clients_user_id', true );
+
+				$data[] = array(
+					'ID'              => $post_id,
+					'client_name'     => Common\cpt_get_client_name( $clients_user_id ),
+					'clients_user_id' => $clients_user_id,
+					'client_id'       => get_user_meta( $clients_user_id, 'cpt_client_id', true ),
+					'sender'          => get_the_author(),
+					'subject'         => get_the_title() ? get_the_title() : '[Message from ' . get_the_author() . ']',
+					'date'            => get_the_date(),
+				);
+		endwhile;
+endif;
+
+		/**
+		 * Pagination
+		 */
+		$total_items  = count( $data );
+		$per_page     = 25;
+		$current_page = $this->get_pagenum();
+		$data         = array_slice( $data, ( ( $current_page - 1 ) * $per_page ), $per_page );
+
+		$this->set_pagination_args(
+			array(
+				'total_items' => $total_items,
+				'per_page'    => $per_page,
+				'total_pages' => ceil( $total_items / $per_page ),
+			)
+		);
+
+		/**
+		 * $this->items contains the data that will actually be displayed on the
+		 * current page.
+		 */
+		$this->items = $data;
+	}
 }
--- admin/cpt-client-manager-table.php
+++ admin/cpt-client-manager-table.php
@@ -6,210 +6,223 @@
  */
 
 namespace Client_Power_Tools\Core\Admin;
+
 use Client_Power_Tools\Core\Common;
 
-class Client_Manager_List_Table extends \WP_List_Table  {
-  function __construct() {
-    global $page;
+class Client_Manager_List_Table extends \WP_List_Table {
+	function __construct() {
+		global $page;
 
-    parent::__construct([
-      'singular'  => 'manager',
-      'plural'    => 'managers',
-      'ajax'      => false,
-    ]);
-  }
+		parent::__construct(
+			array(
+				'singular' => 'manager',
+				'plural'   => 'managers',
+				'ajax'     => false,
+			)
+		);
+	}
 
-  /**
-   * Column Default
-   *
-   * @param array $item A singular item (one full row's worth of data)
-   * @param array $column_name The name/slug of the column to be processed
-   * @return string Text or HTML to be placed inside the column <td>
-   */
-  function column_default($item, $column_name) {
-    return;
-  }
+	/**
+	 * Column Default
+	 *
+	 * @param array $item A singular item (one full row's worth of data)
+	 * @param array $column_name The name/slug of the column to be processed
+	 * @return string Text or HTML to be placed inside the column <td>
+	 */
+	function column_default( $item, $column_name ) {
+		return;
+	}
 
-  /**
-   * Checkboxes
-   *
-   * @see WP_List_Table::::single_row_columns()
-   * @param array $item A singular item (one full row's worth of data)
-   * @return string Text to be placed inside the column <td>
-   */
-  function column_cb($item) {
-    return sprintf(
-      '<input type="checkbox" name="%1$s[]" value="%2$s" />',
-      /* $1%s */ $this->_args['singular'],
-      /* $2%s */ $item['ID']
-    );
-  }
-
-
-  /**
-   * Name Column Method
-   */
-  function column_manager_name($item) {
-    $actions = [
-      'remove' => '<a href="' . add_query_arg(['user_id' => $item['ID'], 'cpt_action' => 'cpt_remove_client_manager']) . '">Remove</a>',
-    ];
-
-    // Return the contents.
-    return sprintf('<strong>%1$s</strong><br />%2$s',
-      /* $1%s */ $item['manager_name'],
-      /* $2%s */ $this->row_actions($actions)
-    );
-  }
+	/**
+	 * Checkboxes
+	 *
+	 * @see WP_List_Table::::single_row_columns()
+	 * @param array $item A singular item (one full row's worth of data)
+	 * @return string Text to be placed inside the column <td>
+	 */
+	function column_cb( $item ) {
+		return sprintf(
+			'<input type="checkbox" name="%1$s[]" value="%2$s" />',
+			/* $1%s */ $this->_args['singular'],
+			/* $2%s */ $item['ID']
+		);
+	}
 
 
-  /**
-   * Clients Column Method
-   */
-  function column_managers_clients($item) {
-    $managers_client_data = cpt_get_managers_clients($item['ID']);
+	/**
+	 * Name Column Method
+	 */
+	function column_manager_name( $item ) {
+		$actions = array(
+			'remove' => '<a href="' . add_query_arg(
+				array(
+					'user_id'    => $item['ID'],
+					'cpt_action' => 'cpt_remove_client_manager',
+				)
+			) . '">Remove</a>',
+		);
 
-    if ($managers_client_data) {
-      foreach ($managers_client_data as $client_data) {
-        $clients_url  = esc_url(admin_url('admin.php?page=cpt'));
-        $client_url   = add_query_arg('user_id', $client_data['user_id'], $clients_url);
-        $client       = '<a href="' . $client_url . '">' . Common\cpt_get_client_name($client_data['user_id']) . '</a>';
-
-        if ($client_data['client_id']) {
-          $client .= ' <span style="color:silver">(' . $client_data['client_id'] . ')</span>';
-        }
-
-        $clients[] = $client;
-      }
-
-      return implode('<br/>' . "\n", $clients);
-    } else {
-      return '<span style="color:silver">None</span>';
-    }
-  }
+		// Return the contents.
+		return sprintf(
+			'<strong>%1$s</strong><br />%2$s',
+			/* $1%s */ $item['manager_name'],
+			/* $2%s */ $this->row_actions( $actions )
+		);
+	}
 
 
-  /**
-   * Get Columns
-   *
-   * @see WP_List_Table::::single_row_columns()
-   * @return array An associative array containing column information:
-   * 'slugs' => 'Visible Titles'.
-   */
-  function get_columns() {
-    $columns = [
-      // 'cb'              => '<input type="checkbox" />',
-      'manager_name'      => 'Manager Name',
-      'managers_clients'  => 'Clients',
-    ];
+	/**
+	 * Clients Column Method
+	 */
+	function column_managers_clients( $item ) {
+		$managers_client_data = cpt_get_managers_clients( $item['ID'] );
 
-    return $columns;
-  }
+		if ( $managers_client_data ) {
+			foreach ( $managers_client_data as $client_data ) {
+				$clients_url = esc_url( admin_url( 'admin.php?page=cpt' ) );
+				$client_url  = add_query_arg( 'user_id', $client_data['user_id'], $clients_url );
+				$client      = '<a href="' . $client_url . '">' . Common\cpt_get_client_name( $client_data['user_id'] ) . '</a>';
 
+				if ( $client_data['client_id'] ) {
+					$client .= ' <span style="color:silver">(' . $client_data['client_id'] . ')</span>';
+				}
 
-  /**
-   * Sortable Columns
-   */
-  function get_sortable_columns() {
-    $sortable_columns = [
-      'manager_name' => ['manager_name', true],
-    ];
+				$clients[] = $client;
+			}
 
-    return $sortable_columns;
-  }
+			return implode( '<br/>' . "\n", $clients );
+		} else {
+			return '<span style="color:silver">None</span>';
+		}
+	}
 
 
-  /**
-   * Bulk Actions
-   *
-   * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
-   */
-  function get_bulk_actions() {
-    return; // Remove this line to enable bulk actions.
+	/**
+	 * Get Columns
+	 *
+	 * @see WP_List_Table::::single_row_columns()
+	 * @return array An associative array containing column information:
+	 * 'slugs' => 'Visible Titles'.
+	 */
+	function get_columns() {
+		$columns = array(
+			// 'cb'              => '<input type="checkbox" />',
+			'manager_name'     => 'Manager Name',
+			'managers_clients' => 'Clients',
+		);
 
-    $actions = [
-      'remove'  => 'remove',
-    ];
-
-    return $actions;
-  }
-
-
-  function process_bulk_action() {
-    $action = $this->current_action();
-
-    switch ($action) {
-      case 'remove':
-        wp_die('Remove client manager permissions.');
-        break;
-
-      default:
-        return;
-        break;
-    }
-
-    return;
-  }
+		return $columns;
+	}
 
 
-  /**
-   * Prepare Data for Display
-   */
-  function prepare_items() {
-    /**
-     * Column Headers
-     */
-    $columns  = $this->get_columns();
-    $hidden   = [];
-    $sortable = $this->get_sortable_columns();
+	/**
+	 * Sortable Columns
+	 */
+	function get_sortable_columns() {
+		$sortable_columns = array(
+			'manager_name' => array( 'manager_name', true ),
+		);
 
-    $this->_column_headers = [$columns, $hidden, $sortable];
-    $this->process_bulk_action();
+		return $sortable_columns;
+	}
 
-    /**
-     * Query Client Managers
-     */
-    $client_managers_query  = new \WP_USER_QUERY([
-      'role'      => 'cpt-client-manager',
-      'orderby'   => 'display_name',
-      'order'     => 'ASC',
-    ]);
-    $client_managers        = $client_managers_query->get_results();
-    $data                   = [];
 
-    // Creates the data set.
-    if (!empty($client_managers)) {
-      foreach ($client_managers as $client_manager) {
-        $data[] = [
-          'ID'            => $client_manager->ID,
-          'manager_name'  => $client_manager->display_name,
-          'manager_email' => $client_manager->user_email,
-        ];
-      }
-    }
+	/**
+	 * Bulk Actions
+	 *
+	 * @return array An associative array containing all the bulk actions: 'slugs'=>'Visible Titles'
+	 */
+	function get_bulk_actions() {
+		return; // Remove this line to enable bulk actions.
 
-    // Sorts the data set.
-    $orderby  = isset($_REQUEST['orderby']) ? sanitize_key($_REQUEST['orderby'])  : 'display_name';
-    $order    = isset($_REQUEST['order'])   ? sanitize_key($_REQUEST['order'])    : 'ASC';
-    $data     = wp_list_sort($data, $orderby, $order);
+		$actions = array(
+			'remove' => 'remove',
+		);
 
-    /**
-     * Pagination
-     */
-    $total_items  = count($data);
-    $per_page     = 25;
-    $current_page = $this->get_pagenum();
-    $data         = array_slice($data, (($current_page - 1) * $per_page), $per_page);
+		return $actions;
+	}
 
-    $this->set_pagination_args([
-      'total_items' => $total_items,
-      'per_page'    => $per_page,
-      'total_pages' => ceil($total_items / $per_page),
-    ]);
 
-    /**
-     * $this->items contains the data that will actually be displayed on the
-     * current page.
-     */
-    $this->items = $data;
-  }
+	function process_bulk_action() {
+		$action = $this->current_action();
+
+		switch ( $action ) {
+			case 'remove':
+				wp_die( 'Remove client manager permissions.' );
+				break;
+
+			default:
+				return;
+				break;
+		}
+
+		return;
+	}
+
+
+	/**
+	 * Prepare Data for Display
+	 */
+	function prepare_items() {
+		/**
+		 * Column Headers
+		 */
+		$columns  = $this->get_columns();
+		$hidden   = array();
+		$sortable = $this->get_sortable_columns();
+
+		$this->_column_headers = array( $columns, $hidden, $sortable );
+		$this->process_bulk_action();
+
+		/**
+		 * Query Client Managers
+		 */
+		$client_managers_query = new \WP_USER_QUERY(
+			array(
+				'role'    => 'cpt-client-manager',
+				'orderby' => 'display_name',
+				'order'   => 'ASC',
+			)
+		);
+		$client_managers       = $client_managers_query->get_results();
+		$data                  = array();
+
+		// Creates the data set.
+		if ( ! empty( $client_managers ) ) {
+			foreach ( $client_managers as $client_manager ) {
+				$data[] = array(
+					'ID'            => $client_manager->ID,
+					'manager_name'  => $client_manager->display_name,
+					'manager_email' => $client_manager->user_email,
+				);
+			}
+		}
+
+		// Sorts the data set.
+		$orderby = isset( $_REQUEST['orderby'] ) ? sanitize_key( $_REQUEST['orderby'] ) : 'display_name';
+		$order   = isset( $_REQUEST['order'] ) ? sanitize_key( $_REQUEST['order'] ) : 'ASC';
+		$data    = wp_list_sort( $data, $orderby, $order );
+
+		/**
+		 * Pagination
+		 */
+		$total_items  = count( $data );
+		$per_page     = 25;
+		$current_page = $this->get_pagenum();
+		$data         = array_slice( $data, ( ( $current_page - 1 ) * $per_page ), $per_page );
+
+		$this->set_pagination_args(
+			array(
+				'total_items' => $total_items,
+				'per_page'    => $per_page,
+				'total_pages' => ceil( $total_items / $per_page ),
+			)
+		);
+
+		/**
+		 * $this->items contains the data that will actually be displayed on the
+		 * current page.
+		 */
+		$this->items = $data;
+	}
 }
