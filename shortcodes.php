8274922165bbd4870e75d6a99776bef4fb524525
samglover <sam@samglover.net>
2024-10-22 00:42:39 -0500

    Convert indents to tabs and apply phpcs formatting

--- shortcodes.php
+++ shortcodes.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace Client_Power_Tools\Core;
+
 use Client_Power_Tools\Core\Common;
 
+add_shortcode( 'status-update-request-button', __NAMESPACE__ . '\status_update_request_button_shortcode' );
 function status_update_request_button_shortcode() {
-  if (!Common\cpt_is_client()) return;
-  ob_start();
-    Common\cpt_status_update_request_button(get_current_user_id());
-  return ob_get_clean();
+	if ( ! Common\cpt_is_client() ) {
+		return;
+	}
+	ob_start();
+		Common\cpt_status_update_request_button( get_current_user_id() );
+	return ob_get_clean();
 }
 
-add_shortcode('status-update-request-button', __NAMESPACE__ . '\status_update_request_button_shortcode');
