013089e95abbba06b1d2e85c7c2af5773aaa2f72
samglover <sam@samglover.net>
2025-02-17 21:09:17 -0600

    Add PHP documentation

--- shortcodes.php
+++ shortcodes.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Shortcodes for Client Power Tools:
+ * - status-update-request-button
+ * - client-dashboard
+ *
+ * @file       shortcodes.php
+ * @package    Client_Power_Tools
+ * @subpackage Core
+ * @since      1.5.2
+ */
 
 namespace Client_Power_Tools\Core;
 
@@ -6,6 +16,13 @@ use Client_Power_Tools\Core\Common;
 use Client_Power_Tools\Core\Frontend;
 
 add_shortcode( 'status-update-request-button', __NAMESPACE__ . '\status_update_request_button_shortcode' );
+/**
+ * Shortcode for inserting the status update request button on the client dashboard.
+ *
+ * @since 1.5.2
+ *
+ * @return string Status update request button HTML.
+ */
 function status_update_request_button_shortcode() {
 	if ( ! Common\cpt_is_client() ) {
 		return;
@@ -17,6 +34,13 @@ function status_update_request_button_shortcode() {
 
 
 add_shortcode( 'client-dashboard', __NAMESPACE__ . '\client_dashboard_shortcode' );
+/**
+ * Shortcode for inserting the client dashboard on the client dashboard page.
+ * Mostly intended for page builders that interfere with the output.
+ *
+ * @since 1.9.2
+ * @return string Client dashboard HTML.
+ */
 function client_dashboard_shortcode() {
 	return Frontend\cpt_get_client_dashboard();
 }
